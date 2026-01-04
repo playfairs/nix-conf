48aa86043b6268f749aef02f9fbfabb0e3dd62e4
playfairs <root@playfairs.cc>
2026-01-04 12:03:22 -0600

    meoq

--- hosts/macmini.nix
+++ hosts/macmini.nix
@@ -13,20 +13,20 @@
   system = {
     dock = {
       autoHideDelay = 3;
-      size = 40;
+      size = 70;
       autohide = true;
       orientation = "bottom";
       entries =
         { pkgs, ... }:
         [
           { path = "/Applications/Zen.app"; }
+          { path = "/Applications/Spotify.app"; }
           { path = "/Applications/Minesweeper.app"; }
           { path = "/Applications/MinesweeperPlus.app"; }
           { path = "/Applications/Discord.app"; }
           { path = "/Applications/Discord Canary.app"; }
           { path = "/Applications/WhatsApp.app"; }
           { path = "/Applications/Telegram.app"; }
-          { path = "/Applications/Spotify.app"; }
           { path = "${pkgs.ghostty-bin}/Applications/Ghostty.app"; }
           { path = "${pkgs.steam}/Applications/Steam.app"; }
         ];
