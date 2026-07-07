9f45389bd5f54c05d1f14ecb5efb4a6c56e12616
playfairs <root@playfairs.cc>
2026-07-06 20:31:03 -0500

    update asteride as inputs

--- hosts/macmini.nix
+++ hosts/macmini.nix
@@ -17,7 +17,7 @@
       autohide = true;
       orientation = "bottom";
       entries =
-        { pkgs, ... }:
+        { pkgs, inputs, ... }:
         [
           { path = "/Applications/Zen.app"; }
           { path = "/Applications/Spotify.app"; }
@@ -26,7 +26,7 @@
           { path = "/Applications/Prism Launcher.app"; }
           { path = "/Applications/Steam.app"; }
           { path = "/Volumes/BARRACUDA/Applications/Xcode.app"; }
-          { path = "${pkgs.asteride}/Applications/AsterIDE.app"; }
+          { path = "${inputs.asteride.packages.${pkgs.system}.asteride}/Applications/AsterIDE.app"; }
         ];
     };
     hostname = "playfairs";
